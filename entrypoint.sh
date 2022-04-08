5974111238717dbe5b751dd9fcd1c86496614f4d
Mahesh Kamsala <mashkams@amazon.com>
2022-04-07 18:37:22 -0700

    first commit

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+set -e
+
+echo "Installing bash"
+apk add --no-cache bash
+
+echo "Installing git & make"
+apk add --no-cache git make
+
+echo "Cloning git-secrets code from github"
+git clone https://github.com/awslabs/git-secrets.git
+
+echo "Installing git-secrets"
+cd git-secrets && make install && cd ..
+
+echo "Running git-secrets"
+git secrets --register-aws --global && git secrets --scan -r .
