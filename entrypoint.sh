2e4f7cf37c0c192757811e513c9e0ee72c6ee8ae
Mahesh Kamsala <mashkams@amazon.com>
2022-04-07 19:13:47 -0700

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ echo "Cloning git-secrets code from github"
 git clone https://github.com/awslabs/git-secrets.git
 
 echo "Installing git-secrets"
-cd git-secrets && make install && cd $GITHUB_WORKSPACE
+cd git-secrets && make install
 
 echo "Running git-secrets"
-git secrets --scan -r .
+git secrets --scan -r $GITHUB_WORKSPACE
