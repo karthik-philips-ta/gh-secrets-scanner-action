f83d8235175e56c963969f77b96d572e2bf7f72b
Karthik Shetty <85149048+karthik-philips-ta@users.noreply.github.com>
2023-05-16 14:05:16 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,20 +32,20 @@ echo "PWD $(pwd)"
 echo "ls $(ls -al)"
 
 echo "Running git-secrets"
-#git secrets --scan
 git-secrets --list
-git secrets --scan 2> secret_logs.txt
-cat secret_logs.txt | grep -q "[ERROR]";
-_secret_exists=$?
+git secrets --scan
+# git secrets --scan 2> secret_logs.txt
+# cat secret_logs.txt | grep -q "[ERROR]";
+# _secret_exists=$?
 
-if [ ${_secret_exists} == 0 ]; then
-    # Secrets exist. Echo error and exit with code 1
-    echo -e '\033[1;31mSecrets exist in your commits. Please rectify the bad commits and re-commit.\033[0m'
-    cat secret_logs.txt
-    rm -rf secret_logs.txt
-    exit 1
-else
-    # Secrets don't exist. Exit with code 0
-    echo -e '\033[1;32mNo secrets exist in your commits.\033[0m'
-    exit 0
-fi 
+# if [ ${_secret_exists} == 0 ]; then
+#     # Secrets exist. Echo error and exit with code 1
+#     echo -e '\033[1;31mSecrets exist in your commits. Please rectify the bad commits and re-commit.\033[0m'
+#     cat secret_logs.txt
+#     rm -rf secret_logs.txt
+#     exit 1
+# else
+#     # Secrets don't exist. Exit with code 0
+#     echo -e '\033[1;32mNo secrets exist in your commits.\033[0m'
+#     exit 0
+# fi 
