c7ca667d27a377f0c83500ef6947786f8a15cf8d
Karthik Shetty <85149048+karthik-philips-ta@users.noreply.github.com>
2023-05-17 14:41:59 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,8 @@ echo "Installing git-secrets"
 cd git-secrets && make install && cd $GITHUB_WORKSPACE
 
 echo "Adding pattern to catch"
-git secrets --add 'password\s*=\s*.+'
+#git secrets --add 'password\s*=\s*.+'
+for pattern in `cat /patterns-prohibit.txt`; do git secrets --add --global "$pattern"; done
 
 echo "Adding pattern to allow"
 for i in $(git show $newrev:.gitallowed 2>/dev/null); do
