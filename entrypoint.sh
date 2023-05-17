0ae4a553dcaa89b55bb823f6deac0bae318826a0
Karthik Shetty <85149048+karthik-philips-ta@users.noreply.github.com>
2023-05-17 09:05:30 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,18 +22,19 @@ if [ "${INPUT_PATTERNTYPE}" == "prohibit" ];then
 elif [ "${INPUT_PATTERNTYPE}" == "allow" ];then
 	#for pattern in `cat /patterns-allow.txt`; do git secrets --add --literal "$pattern"; done
 	patterns_file="/patterns-allow.txt"
-	while IFS= read -r pattern; do
-	      echo "$pattern"
-              #git secrets --add --allowed "$pattern"
-	      git secrets --add --literal "$pattern"
-        done < "$patterns_file"
+	git-secrets --add-provider -- cat /patterns-allow.txt
+# 	while IFS= read -r pattern; do
+# 	      echo "$pattern"
+#               git secrets --add --allowed "$pattern"
+# 	      #git secrets --add --literal "$pattern"
+#         done < "$patterns_file"
 fi    
 
 echo "PWD $(pwd)"
 echo "ls $(ls -al)"
 
 echo "Running git-secrets"
-#git-secrets --list
+git-secrets --list
 git secrets --scan
 git secrets --scan 2> secret_logs.txt
 cat secret_logs.txt | grep -q "[ERROR]";
