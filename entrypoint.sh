ba41113f68644dccffa556ef8fc42af24449f518
Karthik Shetty <85149048+karthik-philips-ta@users.noreply.github.com>
2023-05-17 09:17:26 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,24 +29,29 @@ elif [ "${INPUT_PATTERNTYPE}" == "allow" ];then
         done < "$patterns_file"
 fi    
 
-echo "PWD $(pwd)"
-echo "ls $(ls -al)"
+# echo "PWD $(pwd)"
+# echo "ls $(ls -al)"
 
+#for testing 
+if [ "${INPUT_PATTERNTYPE}" == "allow" ];then
+	for pattern in `cat /patterns-prohibit.txt`; do git secrets --add --global "$pattern"; done
+fi	
+	
 echo "Running git-secrets"
 git-secrets --list
 git secrets --scan
-git secrets --scan 2> secret_logs.txt
-cat secret_logs.txt | grep -q "[ERROR]";
-_secret_exists=$?
+# git secrets --scan 2> secret_logs.txt
+# cat secret_logs.txt | grep -q "[ERROR]";
+# _secret_exists=$?
 
-if [ ${_secret_exists} == 0 ]; then
-    # Secrets exist. Echo error and exit with code 1
-    echo -e '\033[1;31mSecrets exist in your commits. Please rectify the bad commits and re-commit.\033[0m'
-    cat secret_logs.txt
-    rm -rf secret_logs.txt
-    exit 1
-else
-    # Secrets don't exist. Exit with code 0
-    echo -e '\033[1;32mNo secrets exist in your commits.\033[0m'
-    exit 0
-fi 
+# if [ ${_secret_exists} == 0 ]; then
+#     # Secrets exist. Echo error and exit with code 1
+#     echo -e '\033[1;31mSecrets exist in your commits. Please rectify the bad commits and re-commit.\033[0m'
+#     cat secret_logs.txt
+#     rm -rf secret_logs.txt
+#     exit 1
+# else
+#     # Secrets don't exist. Exit with code 0
+#     echo -e '\033[1;32mNo secrets exist in your commits.\033[0m'
+#     exit 0
+# fi 
